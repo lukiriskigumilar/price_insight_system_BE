862528c8bd5a554be1dfac0a7b38149e9c57819f
Luki Riski <contactlukiriski@gmail.com>
2026-04-25 21:05:18 +0700

    feat(category): improve error handling in get all  list categories

--- src/modules/category/category.controller.js
+++ src/modules/category/category.controller.js
@@ -2,14 +2,14 @@ import categoriesService from "./category.service.js";
 import responseHelper from "../../helpers/response.helper.js";
 
 const categoriesController = {
-    GetCategoryList: async (req, res) => {
+    GetCategoryList: async (req, res, next) => {
         try {
             const categories = await categoriesService.getAllCategories();
             responseHelper.sendSuccessResponse(res,"retrived categories successfully", categories, null,200);
         } catch (error) {
-            console.error("Error fetching categories:", error);
-            responseHelper.sendErrorResponse(res,"Failed to fetch categories", error,500);
+            next(error);
         }
+        
     },
     
 }
