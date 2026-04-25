3b77dc74d9c0fec77b277c41c845e01dd3aad9d3
Luki Riski <contactlukiriski@gmail.com>
2026-04-25 21:03:18 +0700

    feat(app): setup error handling

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './src/routes/routes.js';
 import dotenv from 'dotenv';
+import errorMiddleware from './src/middleware/error.middleware.js';
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(errorMiddleware);
 app.use('/api', routes);
 
 app.listen(3000, () => {
