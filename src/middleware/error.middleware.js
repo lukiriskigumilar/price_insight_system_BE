60914cefa0b72ef0c051e7c5028ce4be12517748
Luki Riski <contactlukiriski@gmail.com>
2026-04-26 11:14:37 +0700

    fix(middleware): fix error handling in minddleware to return proper status code and message

--- src/middleware/error.middleware.js
+++ src/middleware/error.middleware.js
@@ -2,7 +2,11 @@ import responseHelper from "../helpers/response.helper.js";
 
 const errorMiddleware = (err, req, res, next) => {
     console.error("Error occurred:", err);
-    responseHelper.sendErrorResponse(res, "An unexpected error occurred", err.message || "Internal Server Error", 500);
+    
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+
+    responseHelper.sendErrorResponse(res, message, [], statusCode);
 }
 
 export default errorMiddleware;
