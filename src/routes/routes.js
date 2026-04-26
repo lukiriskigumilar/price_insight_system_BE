69b337ba1704ec4ba5fe997350ccaa644ace4d2a
Luki Riski <contactlukiriski@gmail.com>
2026-04-26 16:35:01 +0700

    feat(routes): add  product route to global router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import categoryRouter from '../modules/category/category.route.js';
+import productSyncRouter from '../modules/product-sync/product-sync.route.js';
+import productRouter from '../modules/product/product.route.js';
 
 const router = express.Router();
 
 router.use('/categories', categoryRouter);
+router.use('/product-sync', productSyncRouter);
+router.use('/products', productRouter);
 
 export default router;
