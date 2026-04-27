a5c6fb9602a235897285189ea499af705c655d0c
Luki Riski <contactlukiriski@gmail.com>
2026-04-27 09:33:43 +0700

    chore(app): setup CORS

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from 'express';
 import routes from './src/routes/routes.js';
 import dotenv from 'dotenv';
 import errorMiddleware from './src/middleware/error.middleware.js';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 
